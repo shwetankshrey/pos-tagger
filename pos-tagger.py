32e314aa7471ac67769df1e0789be3005ba6fbed
Shwetank Shrey <shwetank16095@iiitd.ac.in>
2018-10-11 16:52:43 +0530

    Created blank file with description for POS Tagger. Also added training set.

---
+++ pos-tagger.py
@@ -0,0 +1,11 @@
+'''
+HMM based POS tagger <NLP Assignment 4>
+
+Author :
+    Shwetank Shrey
+Description :
+    Implements Viterbi Algorithm on a Hidden Markov Model
+    based on a bigram tag / state model using the POS tagged
+    section of the BERP corpus.
+'''
+
